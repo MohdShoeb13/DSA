4730268dddfb0708b7839cd75c026ee2d7433b59
Max Dev <shoebmoh13@gmail.com>
2022-08-15 14:20:21 +0530

    Simple hash Table Implementation

--- com/dsa/adt/SimpleHashTable.java
+++ com/dsa/adt/SimpleHashTable.java
@@ -2,10 +2,44 @@ package com.dsa.adt;
 
 public class SimpleHashTable {
 
-    private String[] hashtable;
+    private Entry[] hashtable;
 
     public SimpleHashTable(){
-        this.hashtable = new String[10];
+        this.hashtable = new Entry[10];
+    }
+
+    private class Entry{
+       private String key;
+       private String value;
+
+        public Entry(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "(" +
+                    "key='" + key + '\'' +
+                    ", value='" + value + '\'' +
+                    ')';
+        }
     }
 
     private int hashKey(String key){
@@ -14,21 +48,84 @@ public class SimpleHashTable {
 
     public void put(String key, String value){
         int hashedKey = hashKey(key);
-        if(hashtable[hashedKey] != null){
+
+        if(isOccupied(hashedKey)){
+            int stopIndex = hashedKey;
+            if(hashedKey == hashtable.length -1){
+                hashedKey = 0;
+            }else {
+                hashedKey++;
+            }
+
+            while (isOccupied(hashedKey) && hashedKey != stopIndex){
+                hashedKey = (hashedKey+1) % hashtable.length;
+            }
+        }
+
+        if(isOccupied(hashedKey)){
             System.err.println("Value is Already present at position " + hashedKey );
         }else {
-            hashtable[hashedKey] = value;
+            hashtable[hashedKey] = new Entry(key,value);
         }
     }
 
-    public String get(String key){
+    private int findKey(String key){
         int hashedKey = hashKey(key);
-        return hashtable[hashedKey];
+        if(hashtable[hashedKey] != null && hashtable[hashedKey].getKey().equals(key)){
+            return hashedKey;
+        }
+        int stopIndex = hashedKey;
+        if(hashedKey == hashtable.length -1){
+            hashedKey = 0;
+        }else {
+            hashedKey++;
+        }
+
+        while (hashedKey != stopIndex && hashtable[hashedKey] != null && !hashtable[hashedKey].getKey().equals(key)){
+            hashedKey = (hashedKey+1) % hashtable.length;
+        }
+
+        if(hashtable[hashedKey] != null && hashtable[hashedKey].getKey().equals(key)){
+            return hashedKey;
+        }else {
+            return -1;
+        }
+    }
+
+    private boolean isOccupied(int index){
+        return hashtable[index] != null;
+    }
+
+    public String get(String key){
+        int hashedKey = findKey(key);
+
+        if(hashedKey == -1){
+            return null;
+        }
+        return hashtable[hashedKey].getValue();
+    }
+
+    public String remove(String key){
+        int hashedKey = findKey(key);
+        if(hashedKey == -1){
+            return null;
+        }
+
+        String value = hashtable[hashedKey].getValue();
+        hashtable[hashedKey] = null;
+        Entry[] oldHashTable = hashtable;
+        hashtable = new Entry[oldHashTable.length];
+        for(int i = 0; i < oldHashTable.length; i++){
+            if(oldHashTable[i] != null){
+                put(oldHashTable[i].getKey(), oldHashTable[i].getValue());
+            }
+        }
+        return value;
     }
 
     public void printHashTable(){
         System.out.print("{");
-        for (String value:
+        for (Entry value:
              hashtable ) {
             if(value!=null){
                 System.out.print(value+",");
@@ -43,8 +140,11 @@ public class SimpleHashTable {
         simpleHashTable.put("Ken", "JAKSJAKJSK");
         simpleHashTable.put("Akuma","99deaths");
         simpleHashTable.put("chun-li","lattee");
+        simpleHashTable.put("12345", "tatty");
 
+        simpleHashTable.remove("12345");
         simpleHashTable.printHashTable();
-        System.out.println(simpleHashTable.get("0000000"));
+        simpleHashTable.get("12345");
+
     }
 }
