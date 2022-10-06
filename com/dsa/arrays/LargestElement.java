7484dfe42b93255f2c66fa1d690753da5825940b
Mohammad Saif <mohammadsaif@Mohammads-MacBook-Pro.local>
2022-10-06 21:52:32 +0530

    largest element in array

---
+++ com/dsa/arrays/LargestElement.java
@@ -0,0 +1,18 @@
+package com.dsa.arrays;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        largest(new int []{1,4,3,2,8,7,6,5,111,23});
+    }
+
+    static void largest(int[] array){
+        int largest = Integer.MIN_VALUE;
+        for (int element : array) {
+            if (element > largest) {
+                largest = element;
+            }
+        }
+        System.out.println("Largest is " + largest);
+
+    }
+}
